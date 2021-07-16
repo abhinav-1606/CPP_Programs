e5ea8c90780567f2883548dc0f53afb3c9eef8ef
abhinav-1606 <adutta1606@gmail.com>
2021-07-17 00:26:54 +0530

    Area

---
+++ Area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the Length = ";
+    cin>>a;
+    cout<<"Enter the Breadth = ";
+    cin>>b;
+    c = a*b;
+    cout<<"The are of the rectangle is = "<<c<<endl;
+    return 0;
+}
